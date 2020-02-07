1a251d877a136d003ecf8082e1f1606cc00b55c7
Student <example@example.com>
2020-02-06 16:19:25 -0800

    generalize naming conventions

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Triangle Tracker',
+      title: 'Title',
       template: './src/index.html',
       inject: 'body'
     })
